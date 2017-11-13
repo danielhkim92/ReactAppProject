afc47543bd786b51dabbfdbab1b23381d316ebf8
danielhkim92 <danielhkim92@gmail.com>
2017-11-13 10:14:42 -0600

    added mvp side bar

--- src/PersonalityTypes/PersonalityTypes.js
+++ src/PersonalityTypes/PersonalityTypes.js
@@ -4,83 +4,85 @@ import React, { Component } from 'react';
 class PersonalityTypes extends Component {
   render() {
     return (
-      <div>
-      	<h3> This is my right side tool bar</h3>
-     		<ul class="collection">
-    			<li class="collection-item avatar">
-			      <img src="images/yuna.jpg" alt="" class="circle"/>
-			      <span class="title">Test</span>
-			      <p>Take your
-			      <br></br>
-			      Test Here
-			      </p>
-      			</li>
-        		<li class="collection-item avatar">
-			      <span class="title">Architect</span>
-			      <p>INTJ</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Logician</span>
-			      <p>INTP</p>
-			    </li>											
-        		<li class="collection-item avatar">
-			      <span class="title">Commander</span>
-			      <p>ENTJ</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Debater</span>
-			      <p>ENTP</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Advocate</span>
-			      <p>INFJ</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Mediator</span>
-			      <p>INFP</p>
-			    </li>											
-        		<li class="collection-item avatar">
-			      <span class="title">Protagonist</span>
-			      <p>ENFJ</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Campaigner</span>
-			      <p>ENFP</p>
-			    </li>        		
-			    <li class="collection-item avatar">
-			      <span class="title">Logistician</span>
-			      <p>ISTJ</p>
-			    </li>        		
-			    <li class="collection-item avatar">
-			      <span class="title">Defender</span>
-			      <p>ISFJ</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Executive</span>
-			      <p>ESTJ</p>
-			    </li>											
-				<li class="collection-item avatar">
-			      <span class="title">Consul</span>
-			      <p>ESFJ</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Virtuoso</span>
-			      <p>ISTP</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Adventurer</span>
-			      <p>ISFP</p>
-			    </li>											
-        		<li class="collection-item avatar">
-			      <span class="title">Entrepreneur</span>
-			      <p>ESTP</p>
-			    </li>
-        		<li class="collection-item avatar">
-			      <span class="title">Entertainer</span>
-			      <p>ESFP</p>
-			    </li>
-  </ul>
-      </div>
+	    <div>
+			<div class="row">
+		      	<div class="col s2">
+		     		<ul class="collection">
+		     		<li class="collection-item avatar">
+					      <span class="title"><h4>Types</h4></span>
+		      			</li>
+		    			<li class="collection-item avatar">
+					      <span class="title">Take Your</span>
+					      <p>Test Here</p>
+		      			</li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Architect</span>
+					      <p>INTJ</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Logician</span>
+					      <p>INTP</p>
+					    </li>											
+		        		<li class="collection-item avatar">
+					      <span class="title">Commander</span>
+					      <p>ENTJ</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Debater</span>
+					      <p>ENTP</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Advocate</span>
+					      <p>INFJ</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Mediator</span>
+					      <p>INFP</p>
+					    </li>											
+		        		<li class="collection-item avatar">
+					      <span class="title">Protagonist</span>
+					      <p>ENFJ</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Campaigner</span>
+					      <p>ENFP</p>
+					    </li>        		
+					    <li class="collection-item avatar">
+					      <span class="title">Logistician</span>
+					      <p>ISTJ</p>
+						    </li>        		
+					    <li class="collection-item avatar">
+					      <span class="title">Defender</span>
+					      <p>ISFJ</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Executive</span>
+					      <p>ESTJ</p>
+					    </li>											
+						<li class="collection-item avatar">
+					      <span class="title">Consul</span>
+					      <p>ESFJ</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Virtuoso</span>
+					      <p>ISTP</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Adventurer</span>
+					      <p>ISFP</p>
+					    </li>											
+		        		<li class="collection-item avatar">
+					      <span class="title">Entrepreneur</span>
+					      <p>ESTP</p>
+					    </li>
+		        		<li class="collection-item avatar">
+					      <span class="title">Entertainer</span>
+					      <p>ESFP</p>
+					    </li>
+		  			</ul>
+	  			</div>
+	  		</div>
+	    </div>	
     );
   }
 }
