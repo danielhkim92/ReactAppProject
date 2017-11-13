f70f1a6057621c707948d03087b5cf17cc743d71
danielhkim92 <danielhkim92@gmail.com>
2017-11-13 09:48:07 -0600

    adding components

---
+++ src/PersonalityTypes/PersonalityTypes.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PersonalityTypes extends Component {
+  render() {
+    return (
+      <div>
+      	<h3> This is my right side tool bar</h3>
+      </div>
+    );
+  }
+}
+
+export default PersonalityTypes;
