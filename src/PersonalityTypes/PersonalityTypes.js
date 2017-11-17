3080c325095ea8a2cc5b221067732ea6b8a9180d
danielhkim92 <danielhkim92@gmail.com>
2017-11-17 04:48:00 -0600

    wow

--- src/PersonalityTypes/PersonalityTypes.js
+++ src/PersonalityTypes/PersonalityTypes.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 
 
 class PersonalityTypes extends Component {
+	CurrentType = (e) => {
+		this.props.containerCurrentType(e.target.innerText);
+	}
   render() {
 
     return (
@@ -12,67 +15,67 @@ class PersonalityTypes extends Component {
 		      			</li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Architect</span><br></br>
-					      <button>INTJ</button>
+					      <button onClick={this.CurrentType}>INTJ</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Logician</span><br></br>
-					      <button>INTP</button>
+					      <button onClick={this.CurrentType}>INTP</button>
 					    </li>											
 		        		<li className="collection-item avatar">
 					      <span className="title">Commander</span><br></br>
-					      <button>ENTJ</button>
+					      <button onClick={this.CurrentType}>ENTJ</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Debater</span><br></br>
-					      <button>ENTP</button>
+					      <button onClick={this.CurrentType}>ENTP</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Advocate</span><br></br>
-					      <button>INFJ</button>
+					      <button onClick={this.CurrentType}>INFJ</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Mediator</span><br></br>
-					      <button>INFP</button>
+					      <button onClick={this.CurrentType}>INFP</button>
 					    </li>											
 		        		<li className="collection-item avatar">
 					      <span className="title">Protagonist</span><br></br>
-					      <button>ENFJ</button>
+					      <button onClick={this.CurrentType}>ENFJ</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Campaigner</span><br></br>
-					      <button>ENFP</button>
+					      <button onClick={this.CurrentType}>ENFP</button>
 					    </li>        		
 					    <li className="collection-item avatar">
 					      <span className="title">Logistician</span><br></br>
-					      <button>ISTJ</button>
+					      <button onClick={this.CurrentType}>ISTJ</button>
 						    </li>        		
 					    <li className="collection-item avatar">
 					      <span className="title">Defender</span><br></br>
-					      <button>ISFJ</button>
+					      <button onClick={this.CurrentType}>ISFJ</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Executive</span><br></br>
-					      <button>ESTJ</button>
+					      <button onClick={this.CurrentType}>ESTJ</button>
 					    </li>											
 						<li className="collection-item avatar">
 					      <span className="title">Consul</span><br></br>
-					      <button>ESFJ</button>
+					      <button onClick={this.CurrentType}>ESFJ</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Virtuoso</span><br></br>
-					      <button>ISTP</button>
+					      <button onClick={this.CurrentType}>ISTP</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Adventurer</span><br></br>
-					      <button>ISFP</button>
+					      <button onClick={this.CurrentType}>ISFP</button>
 					    </li>											
 		        		<li className="collection-item avatar">
 					      <span className="title">Entrepreneur</span><br></br>
-					      <button>ESTP</button>
+					      <button onClick={this.CurrentType}>ESTP</button>
 					    </li>
 		        		<li className="collection-item avatar">
 					      <span className="title">Entertainer</span><br></br>
-					      <button>ESFP</button>
+					      <button onClick={this.CurrentType}>ESFP</button>
 					    </li>
 		  			</ul>
 	  			</div>
--- src/VideoDisplay/VideoDisplay.js
+++ src/VideoDisplay/VideoDisplay.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 
 
 class VideoDisplay extends Component {
+	constructor(props){
+		super(props);
+			this.state = {
+				youtube1: "",
+				youtube2: "",
+				youtube3: ""
+			}
+	}
+
+
 
 
   render() {
+  	const state = this.state;
+  	const x = this.props.display.youtubelink1;
+  	const y = this.props.display.youtubelink2;
+  	const z =	this.props.display.youtubelink3;
+  	state.youtube1 = {x};
+  	state.youtube2 = {y};
+  	state.youtube3 = {z};
+
   	
     return (
       <div className="col s4">
@@ -15,18 +33,18 @@ class VideoDisplay extends Component {
 		  <ul className="collapsible popout">
 		    <li>
 		      <div className="collapsible-header">First</div>
-		      <div className="collapsible-body"><iframe width="420" height="315"
-				src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe></div>
+		      <div className="collapsible-body"><iframe id="1" width="420" height="315"
+				src={x}></iframe></div>
 		    </li>
 		    <li>
 		      <div className="collapsible-header">Second</div>
-		      <div className="collapsible-body"><iframe width="420" height="315"
-		      	src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe></div>
+		      <div className="collapsible-body"><iframe id="2" width="420" height="315"
+		      	src={y}></iframe></div>
 		    </li>
 		    <li>
 		      <div className="collapsible-header">Third</div>
-		      <div className="collapsible-body"><iframe width="420" height="315"
-				src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe></div>
+		      <div className="collapsible-body"><iframe id="3" width="420" height="315"
+				src={z}></iframe></div>
 		    </li>
 		  </ul>
       </div>
